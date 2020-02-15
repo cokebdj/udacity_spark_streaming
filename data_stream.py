0c8a69e124846ded96cc79e6a20458a013e0e20e
cokebdj <alvarojaramillogarcia@gmail.com>
2020-02-15 22:06:19 +0100

    data_stream

--- data_stream.py
+++ data_stream.py
@@ -7,6 +7,19 @@ import pyspark.sql.functions as psf
 
 # TODO Create a schema for incoming resources
 schema = StructType([
+                    StructField('crime_id',LongType()),
+                    StructField('originial_crime_type_name', StringType()),
+                    StructField('report_date', TimestampType()),
+                    StructField('call_date', TimestampType()),
+                    StructField('offense_date', TimestampType()),
+                    StructField('call_time', StringType()),
+                    StructField('call_date_time', TimestampType()),
+                    StructField('disposition', StringType()),
+                    StructField('address', StringType()),
+                    StructField('city', StringType()),
+                    StructField('agency_id', IntegerType()),
+                    StructField('address_type', StringType()),
+                    StructField('common_location', StringType()),
 ])
 
 def run_spark_job(spark):
@@ -16,34 +29,48 @@ def run_spark_job(spark):
     # set up correct bootstrap server and port
     df = spark \
         .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "org.crime.calls") \
+        .load()
 
     # Show schema for the incoming resources for checks
     df.printSchema()
 
     # TODO extract the correct column from the kafka input resources
     # Take only value and convert it to String
-    kafka_df = df.selectExpr("")
+    kafka_df = df.selectExpr("CAST(value as STRING)")
 
     service_table = kafka_df\
         .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
         .select("DF.*")
 
     # TODO select original_crime_type_name and disposition
-    distinct_table = 
+    distinct_table = service_table\
+                     .select('originial_crime_type_name', 'disposition').
+                     .distinct()
 
     # count the number of original crime type
-    agg_df = 
+    agg_df = distinct_table
+             .select('originial_crime_type_name')\
+             .groupby('originial_crime_type_name')\
+             .agg({'originial_crime_type_name':'count'})
 
     # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
     # TODO write output stream
     query = agg_df \
+            .writeStream\
+            .format('console')\
+            .outputMode('Complete')\
+            .trigger(processingTime='30 seconds')\
+            .start()
 
 
     # TODO attach a ProgressReporter
     query.awaitTermination()
 
     # TODO get the right radio code json path
-    radio_code_json_filepath = ""
+    radio_code_json_filepath = "/home/ubuntu/Desktop/udacity/udacity_spark_streaming/radio_code.json"
     radio_code_df = spark.read.json(radio_code_json_filepath)
 
     # clean up your data so that the column names match on radio_code_df and agg_df
@@ -53,7 +80,11 @@ def run_spark_job(spark):
     radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
 
     # TODO join on disposition column
-    join_query = agg_df.
+    join_query = agg_df\
+                 .join(radio_code_df,
+                       col("agg_df.disposition") == col("radio_code_df.disposition"), 
+                       "left_outer" )
+
 
 
     join_query.awaitTermination()
